d5dce8cdb9b005bb20e2ae59338a78a39ad6b8b0
bxker <jake.luke.baker@gmail.com>
2019-08-20 16:05:58 -0500

    finished app 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+//components
+import Todo from './components/Todo'
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      items: [],
+      text: ''
+    }
+    this.handleInput = this.handleInput.bind(this)
+    this.buttonClick = this.buttonClick.bind(this)
+  }
+  // gets input from box and assigns it to the text value in state
+  handleInput(val){
+    this.setState({text: val})
+    // console.log(val)
+  }
+
+  //gets the text value and pushes it into the array
+  buttonClick(){
+    this.setState({items: [...this.state.items, this.state.text]})
+  }
+  
+  
   render() {
+    // assigns a new array to the variable items which will be called in the return(). Passes props to Todo.js
+    let items = this.state.items.map((e, i) => {
+      return <Todo item={e} key={i}/>
+    })
+    // console.log(this.state.items)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <input onChange={ e => this.handleInput(e.target.value)}></input>
+        <button onClick={this.buttonClick}>Add</button>
+        {/* calls the items variable */}
+        {items}
       </div>
     );
   }
